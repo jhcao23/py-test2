47e361888a4fb79cf064e99b0464e6f24aa55466
John Cao <jhcao23@gmail.com>
2019-08-28 21:57:30 -0700

    update

--- deploy.py
+++ deploy.py
@@ -1,4 +1,14 @@
 import requests
+from bs4 import BeautifulSoup
 
 r = requests.get(url='https://www.google.com')
 print(r.headers)
+
+soup = BeautifulSoup("<p>Some<b>bad<i>HTML", "lxml")
+print(soup.prettify())
+soup.find(text="bad")
+soup.i
+
+soup = BeautifulSoup("<tag1>Some<tag2/>bad<tag3>XML", "xml")
+print(soup.prettify())
+
